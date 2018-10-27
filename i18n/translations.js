5aeb5822b4cec120835d868d9ffdf6ebba576593
Yotam Salmon <yotam.salmon@gmail.com>
2018-10-27 15:37:47 +0300

    Hebrew translation i18n

--- i18n/translations.js
+++ i18n/translations.js
@@ -29,6 +29,37 @@ const tr = {
   `
 };
 
+const he = {
+  home: "בית",
+  definitions: "הגדרות",
+  submit: "שלח",
+  "is a deductive reasoning tool": "הוא כלי חשיבה דדוקטיבית",
+  example: "דוגמה",
+  context: "הקשר",
+  argument: "טיעון",
+  language: "שפה",
+  dialect: "ניב",
+  observer: "צופה",
+  therefore: "ולכן",
+  are: "הם:",
+  "are not": "הם אינם",
+  all: "הכל",
+  some: "חלק",
+  no: "לא",
+  "argument form": "טופס טיעון",
+  preview: "תצוגה מקדימה",
+  "new argument": "טיעון חדש",
+  share: "שיתוף",
+  "categorical proposition": "מיקום קטגורי",
+
+  "categorical proposition | definition": `
+    בלוגיקת CILGINIn,הצעה קטגורית היא הצעה
+    שטוענת (או מכחישה) שכל (או חלק)
+    מהחברים בקבוצה מסוימת הם חברים גם
+    בקבוצה אחרת.
+  `
+};
+
 const en = {
   "categorical proposition | definition": `
     In logic, a categorical proposition is a proposition
